8ec48e3d37dc144a68da91a9490b2a1377426070
이건탁 <wow2658@naver.com>
2024-05-21 09:43:20 +0900

    PlayerStart Tag식별 B_TeamSpawningRules
    1. OnChoosePlayerstart (IsA AI인지 아닌지에 따라 GetValidPlayerStart(tag) )
    2. IsValidPlayerStart (tag가 맞고 Claimed가 아니면) -> 근데 Claimed가 뭘까?
    3. GetValidPlayerStart (IsValidPlayerStart true면)

--- Plugins/GameFeatures/ShooterCore/Intermediate/Build/Win64/UnrealEditor/Inc/ShooterCoreRuntime/UHT/ShooterCoreRuntime.init.gen.cpp
+++ Plugins/GameFeatures/ShooterCore/Intermediate/Build/Win64/UnrealEditor/Inc/ShooterCoreRuntime/UHT/ShooterCoreRuntime.init.gen.cpp
@@ -17,7 +17,7 @@ void EmptyLinkFunctionForGeneratedCodeShooterCoreRuntime_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0x5E7A4F70,
+				0x293F8399,
 				0xEBB3397E,
 				METADATA_PARAMS(0, nullptr)
 			};
@@ -25,5 +25,5 @@ void EmptyLinkFunctionForGeneratedCodeShooterCoreRuntime_init() {}
 		}
 		return Z_Registration_Info_UPackage__Script_ShooterCoreRuntime.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_ShooterCoreRuntime(Z_Construct_UPackage__Script_ShooterCoreRuntime, TEXT("/Script/ShooterCoreRuntime"), Z_Registration_Info_UPackage__Script_ShooterCoreRuntime, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x5E7A4F70, 0xEBB3397E));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_ShooterCoreRuntime(Z_Construct_UPackage__Script_ShooterCoreRuntime, TEXT("/Script/ShooterCoreRuntime"), Z_Registration_Info_UPackage__Script_ShooterCoreRuntime, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x293F8399, 0xEBB3397E));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
